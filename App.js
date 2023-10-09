d57f2df5346dd7abcfce7e093fc68dafc9081f0e
Codinglone <codinglone@gmail.com>
2023-10-09 11:46:29 +0200

    Minor changes

--- App.js
+++ App.js
@@ -7,19 +7,28 @@ import { SafeAreaView } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Ratings from "./screens/Ratings";
 import Animations from "./screens/Animations";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
+      <Stack.Navigator>
       <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-        <View>
+        {/* <View>
           <Text>Open up App.js to start working on your app!</Text>
           <Sample />
           <Ratings />
           <Animations />
-          <StatusBar style="auto" />
-        </View>
-      </SafeAreaView>
+          
+        </View>*/}
+        <Stack.Screen name="Home" component={Sample} />
+      <Stack.Screen name="Rating" component={Ratings} />
+      <Stack.Screen name="Animation" component={Animations} />
+      <StatusBar style="auto" />
+      </SafeAreaView> 
+      </Stack.Navigator>
     </SafeAreaProvider>
   );
 }
--- screens/Sample.jsx
+++ screens/Sample.jsx
@@ -1,11 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-const Sample = () => {
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Sample = ({ navigation }) => {
   return (
     <View className="bg-blue-700 text-red-600">
       <SafeAreaView style={{flex: 1}}>
-        <Text>Sample</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Rating')}
+      />
       </SafeAreaView>
     </View>
   );
